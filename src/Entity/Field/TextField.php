99b76cdd127fc0e22ebc450243aa19671002e0f9
Joshua <joshua@lulububu.de>
2024-05-06 17:07:25 +0200

    Generate a password if the defualt is password in the text field

--- src/Entity/Field/TextField.php
+++ src/Entity/Field/TextField.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Bolt\Entity\Field;
 
+use Bolt\Common\Str;
 use Bolt\Entity\Field;
 use Bolt\Entity\FieldInterface;
 use Doctrine\ORM\Mapping as ORM;
@@ -14,4 +15,18 @@ use Doctrine\ORM\Mapping as ORM;
 class TextField extends Field implements Excerptable, FieldInterface
 {
     public const TYPE = 'text';
+
+    /**
+     * Returns the default value option
+     */
+    public function getDefaultValue()
+    {
+        $defaultValue = $this->getDefinition()->get('default', null);
+
+        if ($defaultValue === 'password') {
+            return Str::generatePassword();
+        }
+
+        return $defaultValue;
+    }
 }
